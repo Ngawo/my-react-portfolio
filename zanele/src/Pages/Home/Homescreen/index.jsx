ccbb2b06fdc3e9a866a94b75cce484aa57ffaaba
zanele <zanmangqangwana665@gmail.com>
2024-02-07 23:01:14 +0200

    deleted files and also created the Hero Section

--- zanele/src/Pages/Home/Homescreen/index.jsx
+++ zanele/src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,17 @@
-import HeroSection from "../HeroSection";
+// import HeroSection from "../HeroSection";
 
-export default function Home () {
+export default function Home() {
     return (
-        <>
-        <HeroSection/>
-        </>
-    )
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
+                    <h1 className="section--title">Hello... <br/>I'm ZANELE MANGQANGWANA</h1>
+                    <h3 className="hero--section--tittle--description">A Junior Software Developer</h3>
+                </div>
+                <button className="btn btn-primary">Download Cv</button>
+                <button className="btn btn-primary">Contact Me</button>
+                
+            </div>
+        </section>
+    );
 }
