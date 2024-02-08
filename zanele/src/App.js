fe787f675e3b1f4f9d6c7b3987f3c03c2d3c5178
zanele <zanmangqangwana665@gmail.com>
2024-02-08 02:24:13 +0200

    started working on the navigation bar

--- zanele/src/App.js
+++ zanele/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import NavBar from './Pages/Home/NavBar';
 import Home from './Pages/Home/Homescreen';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+
+      <Router></Router>
  
     </div>
   );
