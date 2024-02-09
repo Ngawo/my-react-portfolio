f085e7fd4efa47996c0b2188cd583b4fdee338da
zanele <zanmangqangwana665@gmail.com>
2024-02-09 20:46:24 +0200

    added an image and styled my home page

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -5,12 +5,16 @@ export default function Home() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <h1 className="section--title">Hello... <br/>I'm ZANELE MANGQANGWANA</h1>
-                    <h3 className="hero--section--tittle--description">A Junior Software Developer</h3>
+                    <p className="section--title">Hello... I'm ZANELE MANGQANGWANA</p>
+                    <h1 className="hero--section--tittle"> <span className="hero--section-title--color">A Junior Software</span> <br />Developer</h1>
+                    <p className="hero--section-description">I am a Junior Software Developer <br/>located in Cape Town.</p>
                 </div>
                 <button className="btn btn-primary">Download Cv</button>
-                <button className="btn btn-primary">Contact Me</button>
+                {/* <button className="btn btn-primary">Contact Me</button> */}
                 
+            </div>
+            <div className="hero--section--img">
+                <img src="src/assets/images/zan.jpg" alt="my pic" />
             </div>
         </section>
     );
